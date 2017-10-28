fbfa2002f4ec2aa0ce70e2e3714541cb35abc9b5
Jordan Kostelidis <JordanKostelidis@JNKSoftware.eu>
2017-10-28 18:38:09 +0300

    Fix Requires paths and SQL Defines

--- API/appConfig.php
+++ API/appConfig.php
@@ -3,15 +3,16 @@
 // System Info
 define("APP_NAME", "DiscountFinder");
 define("APP_DEV", "x-Team");
+
 // Database Info
 define("DB_HOST", "localhost");
-define("DB_USERNAME", "discountFinderUser");
-define("DB_PASSWORD", "PASSWORD");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
 define("DB_DATABASE", "discountfinderdb");
 define("DB_PORT", "3306");
-define("DB_CODE", "UTF8");
+define("DB_CODE", "utf8");
 
 // Include Core Files
-require_once ("../connection/IConnection.php");
-require_once ("../connection/MySQL.php");
-require_once ("../connection/PostgreSQL.php");
+require_once (__DIR__ . "/connection/IConnection.php");
+require_once (__DIR__ . "/connection/MySQL.php");
+require_once (__DIR__ . "/connection/PostgreSQL.php");
